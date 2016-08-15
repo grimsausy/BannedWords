a02a03fb7b2f0ba10cb9cb3914fd944a15382853
Jaus Laird <jaus@scholantis>
2016-08-14 20:37:06 -0700

    fixed the script targets

--- en/AlphabatizeLists.ps1
+++ en/AlphabatizeLists.ps1
@@ -2,6 +2,6 @@ $path = "E:\Projects\BannedWords\en"
 $files = get-childitem "$path\bin\" -Filter *.csv
 
 foreach($file in $files){
-	$import = Import-Csv $file.name | Sort-Object -Descending
-	$import | Export-Csv $file.name
+	$import = Import-Csv "$path\bin\$($file.name)" | Sort-Object -Descending
+	$import | Export-Csv "$path\bin\$($file.name)" -noType
 }
--- en/CreateMasterList.ps1
+++ en/CreateMasterList.ps1
@@ -6,7 +6,7 @@ Remove-Item "$path\Output\Master.csv"
 New-Item "$path\Output\Master.csv" -type file 
 
 foreach($file in $files){
-	$import = Import-Csv $file.name
+	$import = Import-Csv "bin\$($file.name)"
 	
 	if(!($excludeCsv.WordsToExclude.Contains($file.name))){
 		$import | Export-Csv -append "Output\Master.csv" -noType
